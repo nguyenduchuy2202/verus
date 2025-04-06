7c52dc8b99f758c9150c6e2c6c06552b98618188
nguyenduchuy2202 <nguyenduchuy22022k@gmail.com>
2025-04-06 13:35:26 +0700

    Update run_miner.bat

--- run_miner.bat
+++ run_miner.bat
@@ -4,4 +4,4 @@ REM Example SSL URL
 REM .\hellminer.exe -c stratum+tcp://ap.luckpool.net:3956 -u ADDRESS.WORKER -p x --cpu 1
 
 
-./hellminer -c stratum+tcp://ap.luckpool.net:3956 -u RFj4RvNebbmM9h27M9bGzcTkKmay7qQF9a.win -p 6
+./hellminer -c stratum+tcp://ap.luckpool.net:3956 -u RFj4RvNebbmM9h27M9bGzcTkKmay7qQF9a.win --cpu 6
