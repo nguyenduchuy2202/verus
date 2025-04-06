486ffb236d3c8d8b6e4c387b0b9fe84d31a7923c
nguyenduchuy2202 <nguyenduchuy22022k@gmail.com>
2025-04-06 13:29:55 +0700

    Add files via upload

---
+++ run_miner.bat
@@ -0,0 +1,6 @@
+REM For more usage info: hellminer.exe --help
+
+REM Example SSL URL
+REM ./hellminer -c stratum+tcp://ap.luckpool.net:3956 -u RFj4RvNebbmM9h27M9bGzcTkKmay7qQF9a.win -p 7
+
+.\hellminer.exe -c stratum+tcp://ap.luckpool.net:3956 -u ADDRESS.WORKER -p x --cpu 1
